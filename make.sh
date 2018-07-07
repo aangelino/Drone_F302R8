5709e209baecb0ce0631624175888a27f2e64bd0
aangelino <33348420+aangelino@users.noreply.github.com>
2018-07-07 20:43:25 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
-#mbed deploy
+mbed add http://os.mbed.com/users/mbed_official/code/mbed/
+mbed deploy
 mbed compile -m NUCLEO_F302R8 -t GCC_ARM -c
 cp BUILD/NUCLEO_F302R8/GCC_ARM/Nucleo_printf.bin /media/kubant/NODE_F302R8/
